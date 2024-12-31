1cbede339f5212ebe1e885459aa51eb48be08704
Tanisha gupta <tanishagupta1007@gmail.com>
2024-12-31 15:09:33 +0530

    spin the wheel

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="/spinTheWheel.html"
+          // target="_blan
+          // k"
+          rel="noopener noreferrer"
+        >
+          Open My HTML File
+        </a>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
